120d75edfb90cfc581c14462bd87e0681e50cc4f
Tarun Wadhwa <tarunwadhwa13@gmail.com>
2017-10-13 14:57:46 +0530

    Refactored Code.Added class methods

--- spy_details.py
+++ spy_details.py
@@ -1,15 +1,34 @@
 from datetime import datetime
+from passlib.hash import pbkdf2_sha256
 
 class Spy:
-    def __init__(self, name, salutation, age, rating,password):
+    def __init__(self, name, salutation, age, rating):
         self.name = name
         self.salutation = salutation
         self.age = age
         self.rating = rating
+        self.current_status_message = None
+        self.is_online = True
+
+
+class Admin(Spy):
+    def __init__(self, name, salutation, age, rating,password):
+        Spy.__init__(self,name,salutation,age,rating)
         self.is_online = True
         self.chats = []
-        self.current_status_message = None
         self.password = password
+
+    def show_profile(self):
+        print 'Name :' + self.salutation + self.name
+        print 'Age :' + self.age
+        print 'Name :' + self.rating
+        print 'Your Status :' + self.current_status_message
+
+    def change_rating(self):
+        self.rating = float(raw_input("Enter new rating"))
+
+    def change_password(self):
+        self.password = pbkdf2_sha256.hash(raw_input("Enter new password :"))
 
 
 class ChatMessage:
@@ -19,7 +38,5 @@ class ChatMessage:
     self.time = datetime.now()
     self.sent_by_me = sent_by_me
 
-password = '$pbkdf2-sha256$29000$ihEiJMRYCwGA0DrnPMc4xw$0LKeUEGPaVJCF4UZt8ogRxXDXI0YsHmzgO.oWVi30to'
-spy = Spy('bond', 'Mr.', 24, 4.7,password)
 
 friends = []
